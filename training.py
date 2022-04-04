14fc2e245cd9d4c3b049740493a6d2fda39c4fcf
HoagyC <hoagycunningham@gmail.com>
2022-04-04 14:30:52 +0100

    More prints

--- training.py
+++ training.py
@@ -287,12 +287,14 @@ class Memory:
         torch.save(model.state_dict(), path)
 
     def load_model(self, log_dir, model, device=torch.device("cpu")):
+        it = time.time()
         path = os.path.join(log_dir, "latest_model_dict.pt")
         if os.path.exists(path):
             model.load_state_dict(torch.load(path, map_location=device))
         else:
             print(f"no dict to load at {path}")
 
+        print(f"loading time = {time.time() - it}")
         return model
 
     def update_stats(self):
